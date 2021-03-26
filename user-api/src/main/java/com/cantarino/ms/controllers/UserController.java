99c66b5ca87e5a5838a8653027807ad3ec09cb70
Renato Cantarino <cast.carlosc.utic@sebrae.com.br>
2021-03-26 14:40:48 -0300

    evolucao

--- user-api/src/main/java/com/cantarino/ms/controllers/UserController.java
+++ user-api/src/main/java/com/cantarino/ms/controllers/UserController.java
@@ -3,11 +3,10 @@ package com.cantarino.ms.controllers;
 import com.cantarino.ms.dtos.UserDTO;
 import com.cantarino.ms.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,69 +14,37 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class UserController {
 
-    public static List<UserDTO> usuarios = new ArrayList<>();
 
     private final UserService userService;
+    private final String  DELETADO_SUCESSO = "Deletado com sucesso";
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
 
     @GetMapping("/usuarios")
-    public List<UserDTO> getUsuarios()
+    public ResponseEntity<List<UserDTO>> getUsuarios()
     {
-        return userService.All();
+        return ResponseEntity.ok(userService.All());
     }
 
     @GetMapping("/usuarios/{cpf}")
-    public UserDTO getUsuario(@PathVariable String cpf)
+    public ResponseEntity<UserDTO> getUsuario(@PathVariable String cpf)
     {
-        Optional<UserDTO> usuario = usuarios
-                .stream()
-                .filter(c-> c.getCpf().equals(cpf))
-                .findFirst();
-
-        return usuario.orElse(null);
-
+        return ResponseEntity.ok(userService.Get(cpf));
     }
-
-
 
     @DeleteMapping("/usuarios/{cpf}")
-    public void deleteUsuario(@PathVariable String cpf)
+    public ResponseEntity deleteUsuario(@PathVariable String cpf)
     {
-        Optional<UserDTO> usuario = usuarios
-                .stream()
-                .filter(c-> c.getCpf().equals(cpf))
-                .findFirst();
-
-        usuario.ifPresent(userDTODTO -> usuarios.remove(userDTODTO));
+        userService.Remove(cpf);
+        return ResponseEntity.ok().body(DELETADO_SUCESSO);
     }
-
 
     @PostMapping("/usuarios/novo")
-    public UserDTO postUsuario(@RequestBody UserDTO usuario)
+    public ResponseEntity<UserDTO> postUsuario(@RequestBody UserDTO usuario)
     {
-        usuario.setDataCadastro(new Date());
-        usuarios.add(usuario);
-
-        return usuario;
-    }
-
-    @PostConstruct
-    private void Initiate()
-    {
-        UserDTO userDTO = new UserDTO("Eduardo", "123","Rua a",
-                "eduardo@email.com", "1234-3454", new Date());
-
-        UserDTO userDTODTO2 = new UserDTO("Renato", "01174811577","Rua a",
-                "eduardo@email.com", "1234-3454", new Date());
-
-        UserDTO userDTODTO3 = new UserDTO("Lucas", "25687455971","Rua a",
-                "eduardo@email.com", "1234-3454", new Date());
-
-        usuarios.add(userDTO);
-        usuarios.add(userDTODTO2);
-        usuarios.add(userDTODTO3);
+        return new ResponseEntity<>(userService.Add(usuario), HttpStatus.CREATED);
     }
 }
--- user-api/src/main/java/com/cantarino/ms/services/UserService.java
+++ user-api/src/main/java/com/cantarino/ms/services/UserService.java
@@ -3,10 +3,13 @@ package com.cantarino.ms.services;
 import com.cantarino.ms.dtos.UserDTO;
 import com.cantarino.ms.models.User;
 import com.cantarino.ms.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,10 +29,36 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
+    public UserDTO Add(UserDTO usuario) {
+
+        usuario.setDataCadastro(new Date());
+        ModelMapper _mapper = new ModelMapper();
+        User user = _mapper.map(usuario, User.class);
+        userRepository.saveAndFlush(user);
+        return usuario;
+
+
+    }
+
+    public void Remove(String cpf)
+    {
+        Optional<User> user = userRepository.findByCpf(cpf);
+        user.ifPresent(userRepository::delete);
+    }
+
+    public UserDTO Get(String cpf)
+    {
+        Optional<User> user = userRepository.findByCpf(cpf);
+        return user.map(this::ConvertToDTO).orElse(null);
+    }
+
+
+
     private UserDTO ConvertToDTO(User user) {
         return new UserDTO(user.getNome(), user.getCpf() , user.getEndereco(),
                            user.getEmail(), user.getTelefone(), user.getDataCadastro());
     }
+
 
 
 }
